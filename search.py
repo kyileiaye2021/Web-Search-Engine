430affdb25d68856defc4c0d4cda9c42af04f84c
Kyi Lei Aye <klaye@uci.edu>
2026-03-02 21:57:32 -0800

    Update the code structure to make the code more efficient in search query

--- search.py
+++ search.py
@@ -4,6 +4,7 @@ import time
 from collections import defaultdict
 from indexer import preprocess_text
 from decode import decode
+import heapq
 
 BYTE_POSITION_OFFSET_FILE = "byte_position.pkl" 
 MAPPING_FILE = "doc_mapping.pkl"
@@ -44,27 +45,27 @@ def preprocess_query(search_query):
     tokens = preprocess_text(search_query)
     return tokens
     
-def read_postings(token, posting_byte_pos):
-    """
-    Look up the term in posting byte pos and retrieve the posting docs in O(1) time
+# def read_postings(token, posting_byte_pos):
+#     """
+#     Look up the term in posting byte pos and retrieve the posting docs in O(1) time
 
-    Args:
-        token(str): a token or term from a list of user query tokens
-        posting_byte_pos (dict(tuple)): a dictionary of term and its corresponding byte position and len of postings
+#     Args:
+#         token(str): a token or term from a list of user query tokens
+#         posting_byte_pos (dict(tuple)): a dictionary of term and its corresponding byte position and len of postings
 
-    Returns:
-        list(Postings): a list of postings
-    """
-    if token not in posting_byte_pos:
-        return []
+#     Returns:
+#         list(Postings): a list of postings
+#     """
+#     if token not in posting_byte_pos:
+#         return []
     
-    offset, length = posting_byte_pos[token]
-    with open(MERGED_INDEX, "rb") as f:
-        f.seek(offset)
-        byte_raw_data = f.read(length)
-        posting_data = decode(byte_raw_data)
+#     offset, length = posting_byte_pos[token]
+#     with open(MERGED_INDEX, "rb") as f:
+#         f.seek(offset)
+#         byte_raw_data = f.read(length)
+#         posting_data = decode(byte_raw_data)
         
-    return posting_data
+#     return posting_data
 
 def intersect(p1, p2):
     """
@@ -94,24 +95,47 @@ def intersect(p1, p2):
 
 def search_query(query_tokens, posting_byte_pos, doc_mapping, top_k=5):
     """
-    Search and rank documents by TF-IDF
+    Look up the term in posting byte pos and retrieve the posting docs in O(1) time
+    Apply Boolean retrieval method to extract common postings among queries
+    Search and rank documents by TF-IDF on the list of common postings
+
+    Args:
+        query_token(str): a token or term from a list of user query tokens
+        posting_byte_pos (dict(tuple)): a dictionary of term and its corresponding byte position and len of postings
+        doc_mapping (dict(str)): a dictionary of term and its corresponding doc
+        top_k (int): num of k links return
+
+    Returns:
+        list(dict(str: score)): a list of dictionary {url: score}
     """
     
     total_docs = len(doc_mapping)
 
     #Get all token postings
     all_postings = []
-    for token in query_tokens:
-        postings = read_postings(token, posting_byte_pos)
-        if not postings:
-            return [] #means not find any word
-        all_postings.append(postings)
-
-    #Find documents that contain all query tokens.
-    if len(all_postings) == 0:
-        return []
     
-    #start with shortest posting list
+    # Open stored index file
+    with open(MERGED_INDEX, 'rb') as f:
+        # read postings once per token
+        for token in query_tokens:
+            # postings = read_postings(token, posting_byte_pos)
+            if token not in posting_byte_pos:
+                return []
+    
+            offset, length = posting_byte_pos[token]
+            f.seek(offset)
+            byte_raw_data = f.read(length)
+            postings = decode(byte_raw_data)
+                
+            if not postings:
+                return [] #means not find any word
+            all_postings.append(postings)
+
+        #Find documents that contain all query tokens.
+        if len(all_postings) == 0:
+            return []
+    
+    #start with shortest posting list to do Boolean Retrieval model in O(m + n) time
     all_postings.sort(key=len)
     result = all_postings[0]
     for i in range(1, len(all_postings)):
@@ -123,16 +147,19 @@ def search_query(query_tokens, posting_byte_pos, doc_mapping, top_k=5):
     valid_doc_ids = set(p[0] for p in result)
     scores = defaultdict(float)
     
-    for token in query_tokens:
-        postings = read_postings(token, posting_byte_pos)
+    for postings in all_postings:
         df = len(postings)
         
+        if df == 0:
+            continue
+        
+        idf = math.log(total_docs / df)
+            
         for doc_id, tf, is_important in postings:
             if doc_id not in valid_doc_ids:
                 continue
                 
-            if tf > 0 and df > 0:
-                idf = math.log(total_docs / df)
+            if tf > 0 and idf > 0:
                 tfidf = (1 + math.log(tf)) * idf
                 
                 #import will earn 1.5x weight
@@ -141,9 +168,10 @@ def search_query(query_tokens, posting_byte_pos, doc_mapping, top_k=5):
                 
                 scores[doc_id] += tfidf
 
-    ranked = sorted(scores.items(), key=lambda x: x[1], reverse=True)
+    # ranked = sorted(scores.items(), key=lambda x: x[1], reverse=True)
+    ranked = heapq.nlargest(top_k, scores.items(), key=lambda x: x[1])
     results = []
-    for doc_id, score in ranked[:top_k]:
+    for doc_id, score in ranked:
         url = doc_mapping.get(doc_id, "Unknown URL")
         # results.append((url, score))
         results.append({
@@ -176,7 +204,7 @@ def main():
         print(f"Response time: {response_time:.2f} ms")
 
         if not results:
-            print("not fing any result")
+            print("not finding any result")
         else:
             print(f"\nSearch Results (Top 5):")
             for i, res in enumerate(results, 1):
